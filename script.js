7971d46bada4b7aad687b9a7bf5f4bb259d39f1c
MuneerAhmed07 <105144687+MuneerAhmed07@users.noreply.github.com>
2022-08-31 20:44:50 +0500

    future-institute

---
+++ script.js
@@ -0,0 +1,36 @@
+$("#menu-btn").click(function(){
+    $('nav .navigation ul').addClass("active");
+});
+
+$("#menu-close").click(function(){
+    $('nav .navigation ul').removeClass("active");
+});
+
+//   for review section 
+
+var swiper = new Swiper(".review-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+    },
+  });
